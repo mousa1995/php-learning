31a31757b0fa40513aa494e47d042b7b5d293d1d
mousa1995 <mousasoccerlove@gmail.com>
2026-09-07 20:36:20 +0330

    learn: php null and use of var_dump for type checking

---
+++ drills/types/05-null.php
@@ -0,0 +1,14 @@
+<?php
+
+// Null and zero and empty string are different in PHP
+$value1 = null;
+$value2 = 0;
+$value3 = "";
+
+
+$name = null;
+$age = 0;
+$job = "";
+
+// We can use var_dump for type checking 
+var_dump($name, $age, $job);
