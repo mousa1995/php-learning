a5d7c6cd5cb3b9b3c67e59f7ce3ebec0bb7abdd6
mousa1995 <mousasoccerlove@gmail.com>
2026-09-07 21:51:03 +0330

    learn: php arithmetic operators

---
+++ drills/operators/01-arithmetic.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 10;
+$b = 3;
+
+// Arithmetic operators
+
+var_dump($a + $b);  // addition
+var_dump($a - $b);  // subtraction
+var_dump($a * $b);  // multiplication
+var_dump($a / $b);  // division
+var_dump($a % $b);  // remainder
+var_dump($a ** $b); // exponentiation
