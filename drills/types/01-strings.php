73cf23f2ecc5c4fc79fc390b77afd1f0d2b7e35b
mousa1995 <mousasoccerlove@gmail.com>
2026-09-07 19:45:24 +0330

    learn: php strings

---
+++ drills/types/01-strings.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "Mousa";
+$city = "Rome";
+$job = "Developer";
+
+echo "$name lives in $city";
+
+echo $name . " is a " . $job;
+
+
+
+
+echo 'Hello $name'; // Single quotes don't support interpolation
+
+echo "\n";
+
+echo "Hello $name"; // Double quotes support interpolation
