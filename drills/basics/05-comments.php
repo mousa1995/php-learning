72fd68abcf25758ea359559494d576f33950033c
mousa1995 <mousasoccerlove@gmail.com>
2026-09-07 19:36:20 +0330

    learn: php comments

---
+++ drills/basics/05-comments.php
@@ -0,0 +1,13 @@
+<?php
+
+// this is a one line comment
+
+# this is aouther one line comment
+
+/*
+    this
+        is
+            several 
+                   line
+                       comment 
+*/
