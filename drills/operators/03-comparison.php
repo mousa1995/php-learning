947e13b69c74bcbeb984f694b1a46e38195b883d
mousa1995 <mousasoccerlove@gmail.com>
2026-09-08 10:02:27 +0330

    learn: php comparison operators

---
+++ drills/operators/03-comparison.php
@@ -0,0 +1,13 @@
+<?php
+
+$a = 10;
+$b = 5;
+
+var_dump($a == $b);   // equal
+var_dump($a != $b);   // not equal
+var_dump($a === $b);  // identical
+var_dump($a !== $b);  // not identical
+var_dump($a > $b);    // greater than
+var_dump($a < $b);    // less than
+var_dump($a >= $b);   // greater than or equal
+var_dump($a <= $b);   // less than or equal
