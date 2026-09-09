34a7bc2b4d7bb7ffce5d8f5ccba195818ba17c69
mousa1995 <mousasoccerlove@gmail.com>
2026-09-09 13:52:46 +0330

    learn : php arrays

---
+++ drills/arrays/05-iterate-arrays.php
@@ -0,0 +1,17 @@
+<?php
+
+$numbers = [10, 20, 30, 40];
+
+foreach ($numbers as $number) {
+    echo $number . "\n";
+}
+
+$user = [
+    "name" => "Mousa",
+    "age" => 32,
+    "job" => "Developer",
+];
+
+foreach ($user as $key => $value) {
+    echo "$key: $value\n";
+}
