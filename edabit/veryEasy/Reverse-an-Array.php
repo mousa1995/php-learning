f9c3a253295df4830de0d0a4977c4c60ce0f509f
mousa1995 <mousasoccerlove@gmail.com>
2026-09-10 15:11:20 +0330

    solve: (very easy) reverse an array

---
+++ edabit/veryEasy/Reverse-an-Array.php
@@ -0,0 +1,11 @@
+<?php
+
+#https://edabit.com/challenge/4w5pz4bAHimj7HiHb
+function reverse($arr)
+{
+    return array_reverse($arr);
+}
+
+// echo reverse([1, 2, 3, 4]); // ➞ [4, 3, 2, 1]
+// echo reverse([9, 9, 2, 3, 4]); // ➞ [4, 3, 2, 9, 9]
+// echo reverse([]); // ➞ []
