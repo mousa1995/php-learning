30377c895733fcb9883d30dd7c9256b542a03fce
mousa1995 <mousasoccerlove@gmail.com>
2026-09-10 06:05:18 +0330

    solve: (very easy) howManySeconds

---
+++ edabit/veryEasy/Convert-Hours-into-Seconds.php
@@ -0,0 +1,8 @@
+<?php
+
+#https://edabit.com/challenge/6Q9QLnFf73Cp4QwRK
+
+function howManySeconds($hours)
+{
+    return $hours * (60 ** 2);
+}
---
+++ edabit/veryEasy/Return-theFirst-Element-in-an-Array.php
@@ -0,0 +1,8 @@
+<?php
+
+//https://edabit.com/challenge/dao7bqK4fuzTudDdB
+
+function getFirstValue($array)
+{
+    return $array[0];
+}
