3d8127ddd0160710c86abf21097d524834e42c29
mousa1995 <mousasoccerlove@gmail.com>
2026-09-10 08:24:18 +0330

    learn: PHP POST requests

---
+++ src/forms-and-input/post.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <form method="POST">
+        <input name="name">
+        <button type="submit">Send</button>
+    </form>
+
+    <?php
+
+    //echo $_POST["name"];
+    var_dump($_POST)
+
+    ?>
+
+</body>
+
+</html>
