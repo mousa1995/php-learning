0b25444121a59a23fe270f02e3e965dd627a2bce
mousa1995 <mousasoccerlove@gmail.com>
2026-09-17 14:03:28 +0330

    learn: is_bool PHP

---
+++ src/types/variableAndTypeFunctions/is_bool/01-is_bool.php
@@ -0,0 +1,12 @@
+<?php
+$isLoggedIn = false;
+
+var_dump(is_bool($isLoggedIn));
+
+
+// important 
+is_bool(true);  // true
+is_bool(false); // true
+
+is_bool(1);     // false
+is_bool(0);     // false
