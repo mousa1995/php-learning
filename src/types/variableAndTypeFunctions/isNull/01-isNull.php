49161aa52669b24ad1705548f3a22a650e310836
mousa1995 <mousasoccerlove@gmail.com>
2026-09-17 10:27:12 +0330

    learn: is_null PHP

--- src/types/variableAndTypeFunctions/isNull/01-isNull.php
+++ src/types/variableAndTypeFunctions/isNull/01-isNull.php
@@ -1,1 +1,26 @@
 <?php
+
+// Checks if a value is Null or not 
+
+// example
+$name = null;
+
+var_dump(is_null($name)); // true
+
+// example 2 
+$name = "Mousa";
+
+var_dump(is_null($name)); // false
+
+// concise
+$a = 0;
+$b = "";
+$c = false;
+$d = [];
+$e = null;
+
+is_null($a); // false
+is_null($b); // false
+is_null($c); // false
+is_null($d); // false
+is_null($e); // true
