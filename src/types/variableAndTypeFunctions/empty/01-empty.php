1766800c5b12dcea9e02d946ea042c20ca43ac5f
mousa1995 <mousasoccerlove@gmail.com>
2026-09-17 09:53:11 +0330

    learn: empty

--- src/types/variableAndTypeFunctions/empty/01-empty.php
+++ src/types/variableAndTypeFunctions/empty/01-empty.php
@@ -1,1 +1,19 @@
 <?php
+//Empty is equal to => !isset($var) || $var == false 
+
+
+//example
+$user = [
+    "profile" => [
+        "name" => "Mousa"
+    ]
+];
+
+if (empty($user["profile"]["email"])) {
+    echo "Email is empty";
+} //  => email is empty 
+
+// note "" is empty
+// " " is NOT empty! 
+
+// example 2
